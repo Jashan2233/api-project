034db926fcc29a20069c3841a495a1446a9a071a
Jashan2233 <jashan17100@gmail.com>
2023-08-05 18:17:15 -0500

    Merge dev branch into main

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,7 +5,7 @@ const config = require('./index');
 module.exports = {
   development: {
     storage: config.dbFile,
-    dialect: 'sqlite',
+    dialect: 'postgres',
     seederStorage: "sequelize",
     logQueryParameters: true,
     typeValidation: true
