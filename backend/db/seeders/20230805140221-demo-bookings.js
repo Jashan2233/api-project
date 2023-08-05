ea02b8aacbe58aaee967ef527a736dab7a7fdbdf
Jashan2233 <jashan17100@gmail.com>
2023-08-05 09:15:01 -0500

    validating set to true on demo bookings and adding options object

--- backend/db/seeders/20230805140221-demo-bookings.js
+++ backend/db/seeders/20230805140221-demo-bookings.js
@@ -1,6 +1,8 @@
 'use strict';
 let options = {};
-/** @type {import('sequelize-cli').Migration} */
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
 module.exports = {
   async up (queryInterface, Sequelize) {
     /**
@@ -37,7 +39,7 @@ module.exports = {
         startDate: "2022-12-1",
         endDate: "2022-12-5"
       },
-    ])
+    ], {validate: true})
   },
 
   async down (queryInterface, Sequelize) {
@@ -47,7 +49,7 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-
-    await queryInterface.bulkDelete('Bookings', null, {})
+options.tableName = 'Bookings';
+    await queryInterface.bulkDelete(options, null, {})
   }
 };
