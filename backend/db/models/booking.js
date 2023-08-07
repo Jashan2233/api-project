a2e28d91ae3fbb6d74ba0cb17b237425f93cd799
Jashan2233 <jashan17100@gmail.com>
2023-08-07 11:01:16 -0500

    added model,seeder,migrations for bookings,spots

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -11,19 +11,25 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Booking.belongsTo(
-        models.spot, {
-        foreignKey: 'spotId'
-      }
-      );
-      Booking.belongsTo(
-        models.User, {
-          foreignKey: 'userId'
-        }
-      )
+      Booking.belongsTo(models.Spot, {
+        foreignKey: 'spotId',
+        // onDelete: 'CASCADE',
+        // hooks: true
+      })
+      Booking.belongsTo(models.User, {
+        foreignKey: 'userId',
+        // onDelete: 'CASCADE',
+        // hooks: true
+      })
     }
   }
   Booking.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
     spotId: {
       type: DataTypes.INTEGER,
       allowNull: false
@@ -32,24 +38,11 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       allowNull: false
     },
-    startDate: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      validate: {
-        isDate: true
-      }
-    },
-    endDate: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      validate: {
-        isDate: true
-      }
-    }
+    startDate: DataTypes.DATE,
+    endDate: DataTypes.DATE
   }, {
     sequelize,
     modelName: 'Booking',
-    tableName: 'Bookings'
   });
   return Booking;
 };
--- backend/db/seeders/20230805140221-demo-bookings.js
+++ backend/db/seeders/20230805140221-demo-bookings.js
@@ -1,58 +1,89 @@
 'use strict';
+const {Booking} = require('../models')
 
-const  { Booking } = require ('../models')
+let options = {}
 
-let options = {};
+
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
+
+/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
     await Booking.bulkCreate([
-      {
-        spotId: 1,
-        userId: 3,
-        startDate: "2022-11-1",
-        endDate: "2022-11-5"
-      },
-      {
-        spotId: 2,
-        userId: 3,
-        startDate: "2022-12-1",
-        endDate: "2022-12-5"
-      },
-      {
-        spotId: 3,
-        userId: 1,
-        startDate: "2022-10-30",
-        endDate: "2022-11-8"
-      },
-      {
-        spotId: 1,
-        userId: 4,
-        startDate: "2022-12-1",
-        endDate: "2022-12-5"
-      },
-    ], {validate: true})
+        {
+            spotId: 1,
+            userId: 1,
+            startDate: '2023-09-01',
+            endDate: '2023-09-03',
+        },
+        {
+            spotId: 2,
+            userId: 1,
+            startDate: '2023-09-05',
+            endDate: '2023-09-10',
+        },
+        {
+            spotId: 3,
+            userId: 1,
+            startDate: '2023-09-15',
+            endDate: '2023-09-20',
+        },
+        {
+            spotId: 4,
+            userId: 1,
+            startDate: '2023-09-22',
+            endDate: '2023-09-26',
+        },
+        {
+            spotId: 5,
+            userId: 1,
+            startDate: '2023-10-01',
+            endDate: '2023-10-05',
+        },
+        {
+            spotId: 6,
+            userId: 1,
+            startDate: '2023-10-10',
+            endDate: '2023-10-15',
+        },
+
+        {
+            spotId: 7,
+            userId: 2,
+            startDate: '2023-09-05',
+            endDate: '2023-09-09',
+        },
+        {
+            spotId: 8,
+            userId: 2,
+            startDate: '2023-09-12',
+            endDate: '2023-09-18',
+        },
+        {
+            spotId: 9,
+            userId: 2,
+            startDate: '2023-09-20',
+            endDate: '2023-09-25',
+        },
+        {
+            spotId: 10,
+            userId: 2,
+            startDate: '2023-09-30',
+            endDate: '2023-10-03',
+        },
+        {
+            spotId: 11,
+            userId: 2,
+            startDate: '2023-10-07',
+            endDate: '2023-10-11',
+        }
+    ], options)
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-options.tableName = 'Bookings';
-    await queryInterface.bulkDelete(options, null, {})
+    options.tableName = 'Bookings'
+  await queryInterface.bulkDelete(options, null)
   }
 };
