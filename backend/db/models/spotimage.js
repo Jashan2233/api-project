0ea310c5485798d0f7ec008e9a01a2339bf79c44
Jashan2233 <jashan17100@gmail.com>
2023-08-09 11:35:41 -0500

    ss

--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -9,6 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+    //define models
     static associate(models) {
       // define association here
       SpotImage.belongsTo(models.Spot, {
