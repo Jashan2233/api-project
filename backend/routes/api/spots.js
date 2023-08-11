fbf732d1657786976db985958826bcd004c35b1e
Jashan2233 <jashan17100@gmail.com>
2023-08-10 19:03:43 -0500

    fixed bug with ownerId

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -135,7 +135,7 @@ router.get('/current', requireAuth, async (req, res) => {
 
    const spots = await Spot.findAll({
     where: {
-        ownerid: id
+        ownerId: id
     },
     attributes: {
         include: [
@@ -266,7 +266,7 @@ router.post('/', requireAuth, validateCreateSpot, async (req, res, next) => {
     res.json(spot)
 })
 
-// Add an Image to a spoit with SpotId!
+// Add an Image to a spoit with SpotId -- fixed!
 
 router.post('/:spotId/images', requireAuth, requireAuthor, async (req, res, next) => {
     const spotId = req.params.spotId;
