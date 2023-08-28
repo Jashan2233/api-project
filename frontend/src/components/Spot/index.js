bbd53e974d9df8932de817b400335d054b8d3caa
Jashan2233 <jashan17100@gmail.com>
2023-08-28 11:41:01 -0500

    implemented get all Spots at '/'

---
+++ frontend/src/components/Spot/index.js
@@ -0,0 +1,51 @@
+import { useDispatch, useSelector } from "react-redux";
+import { getAllSpotsThunk } from "../../store/spot";
+import { NavLink } from "react-router-dom";
+import { useEffect } from "react";
+import "./Spot.css";
+
+const Spots = () => {
+  const dispatch = useDispatch();
+  const spots = useSelector((state) => state.spots);
+  const allSpots = Object.values(spots.allSpots);
+
+  useEffect(() => {
+    dispatch(getAllSpotsThunk());
+  }, [dispatch]);
+
+  return (
+    <>
+      <div className="spots-container">
+        {allSpots.map((spot) => (
+          <NavLink to={`/spots/${spot.id}`}>
+            <div className="ind-spot">
+              <div className="tooltip">
+                <img id="spot-img" src={`${spot.previewImage}`} alt="img" />
+                <span className="tooltiptext">{spot.name}</span>
+              </div>
+              <div className="under-img">
+                <div className="city">
+                  {spot.city}, {spot.state}
+                </div>
+                {spot.avgRating ? (
+                  <div className="review">
+                    <b>{spot.avgRating.toFixed(1)}</b>
+                  </div>
+                ) : (
+                  <div className="review">
+                    <b>New</b>
+                  </div>
+                )}
+              </div>
+              <div className="price">
+                <b>${spot.price}</b> night
+              </div>
+            </div>
+          </NavLink>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Spots;
--- frontend/src/store/spot.js
+++ frontend/src/store/spot.js
@@ -1,6 +1,6 @@
 import { csrfFetch } from "./csrf";
 
-// Types
+// TYPES
 const GET_ALL_SPOTS = "spot/getAllSpots";
 const GET_SPOT = "spot/getSpot";
 const CREATE_SPOT = "spot/createSpot";
@@ -8,8 +8,7 @@ const UPDATE_SPOT = "spot/updateSpot";
 const CREATE_SPOT_IMAGE = "spot/createSpotImage";
 const DELETE_SPOT = "spot/deleteSpot";
 
-// ACTION!
-
+// ACTIONS
 const getAllSpots = (spots) => {
   return {
     type: GET_ALL_SPOTS,
@@ -45,9 +44,7 @@ const deleteSpot = (spotId) => {
   };
 };
 
-// Thunks
-
-//Get all Spots
+// ACTION THUNKS
 export const getAllSpotsThunk = () => async (dispatch) => {
   const response = await csrfFetch("/api/spots");
 
@@ -55,10 +52,10 @@ export const getAllSpotsThunk = () => async (dispatch) => {
     let spots = await response.json();
     spots = spots.Spots;
     dispatch(getAllSpots(spots));
+    return spots;
   }
 };
 
-//Get One Spot
 export const getSpotThunk = (spotId) => async (dispatch) => {
   const response = await csrfFetch(`/api/spots/${spotId}`);
 
@@ -68,3 +65,115 @@ export const getSpotThunk = (spotId) => async (dispatch) => {
     return spot;
   }
 };
+
+export const createSpotThunk = (spot, img) => async (dispatch) => {
+  const response = await csrfFetch("/api/spots", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(spot),
+  });
+
+  if (response.ok) {
+    const newSpot = await response.json();
+    for (let i = 0; i < img.length; i++) {
+      if (img[i]) {
+        await csrfFetch(`/api/spots/${newSpot.id}/images`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            url: img[i],
+            preview: true,
+          }),
+        });
+      }
+    }
+    dispatch(createSpot(newSpot));
+    return newSpot;
+  }
+};
+
+export const updateSpotThunk = (spot, spotId) => async (dispatch) => {
+  const response = await csrfFetch(`/api/spots/${spotId}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(spot),
+  });
+
+  if (response.ok) {
+    const spot = await response.json();
+    dispatch(updateSpot(spot));
+    return spot;
+  }
+};
+
+export const deleteSpotThunk = (spotId) => async (dispatch) => {
+  const response = await csrfFetch(`/api/spots/${spotId}`, {
+    method: "DELETE",
+  });
+
+  if (response.ok) {
+    dispatch(deleteSpot(spotId));
+    // return true
+  }
+};
+
+// INITIAL STATE
+const initialState = { allSpots: {}, singleSpot: { SpotImages: [] } };
+
+// REDUCER
+const spotReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_ALL_SPOTS: {
+      let allSpots = {};
+      action.spots.forEach((spot) => (allSpots[spot.id] = spot));
+      return { allSpots: { ...allSpots }, singleSpot: { ...state.singleSpot } };
+    }
+    case GET_SPOT: {
+      let newState = { ...state, singleSpot: { ...state.singleSpot } }; //creates copy of state and the key we're going to change
+      let oneSpot = {
+        ...action.spot,
+        SpotImages: [state.singleSpot.SpotImages],
+      }; //spreads fetched spot data, and copies spot images (currently empty)
+      action.spot.SpotImages.forEach((image, i) => {
+        //iterates through fetched spot data and keys into images, iterates and assigns each image to empty spot images array
+        oneSpot.SpotImages[i] = image;
+      });
+      newState.singleSpot = oneSpot; //reassigns spot/spot images into single spot in new copy
+      return newState;
+    }
+    case CREATE_SPOT: {
+      let singleSpot = {};
+      singleSpot = { ...action.spot };
+      let newState = { ...state, singleSpot };
+      newState.allSpots[action.spot.id] = { ...action.spot };
+
+      return newState;
+    }
+    case UPDATE_SPOT: {
+      const newState = {
+        ...state,
+        allSpots: { ...state.allSpots },
+        singleSpot: { ...state.singleSpot },
+      };
+      // console.log(newState)
+      newState.allSpots[action.spot.id] = {
+        ...newState.allSpots[action.spotId],
+        ...action.spot,
+      };
+    }
+    case DELETE_SPOT: {
+      const newState = {
+        ...state,
+        allSpots: { ...state.allSpots },
+        singleSpot: { ...state.singleSpot },
+      };
+      delete newState.allSpots[action.spotId];
+      delete newState.singleSpot[action.spotId];
+      return newState;
+    }
+    default:
+      return state;
+  }
+};
+
+export default spotReducer;
