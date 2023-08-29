576bbdae1f4c4e9dfde69e79454a0acffeff5d02
Jashan2233 <jashan17100@gmail.com>
2023-08-29 13:01:40 -0500

    added css stylings, fixed css bugs

---
+++ backend/db/seeders/20230829174230-demo-spotimages.js
@@ -0,0 +1,51 @@
+"use strict";
+const { SpotImage } = require("../models");
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await SpotImage.bulkCreate(
+      [
+        {
+          spotId: 1,
+          url: "https://www.nztravelorganiser.com/wp-content/uploads/2019/09/hobbiton-1024x683.jpg",
+          preview: true,
+        },
+        {
+          spotId: 2,
+          url: "https://i.imgur.com/wRS2Shv.jpg",
+          preview: true,
+        },
+        {
+          spotId: 3,
+          url: "https://i.imgur.com/mXZX1nm.jpg",
+          preview: true,
+        },
+        {
+          spotId: 4,
+          url: "https://i.imgur.com/uOEj82R.jpg",
+          preview: true,
+        },
+        {
+          spotId: 6,
+          url: "https://i.imgur.com/drqU2Ls.jpg",
+          preview: true,
+        },
+        {
+          spotId: 9,
+          url: "https://www.ancient-origins.net/sites/default/files/field/image/Mount-Olympus.jpg",
+          preview: true,
+        },
+      ],
+      options
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    options.tableName = "SpotImages";
+    await queryInterface.bulkDelete(options);
+  },
+};
